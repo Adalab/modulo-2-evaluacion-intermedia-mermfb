61d0422f644f50e3b327a6b03bcf0448fc964e3c
Mercedes <mermfb@gmail.com>
2021-01-28 10:59:37 +0100

    Add handleGame function

--- js/main.js
+++ js/main.js
@@ -4,8 +4,11 @@
 const tryButtonElement= document.querySelector('.container__form--button');
 const counterElement= document.querySelector('.container__attempt');
 const randomNumber= getRandomNumber(100);
+const introduceNumber= document.querySelector('.container__form--number');
+const clue= document.querySelector('.container__clue');
 
-//ESCRIBIR NUMERO DE INTENTOS
+
+
 
 //GENERAR NÚMERO ALEATORIO
 
@@ -15,8 +18,25 @@ function getRandomNumber(max) {
     console.log(`El número aleatorio es ${randomNumber}`);
 
 //LEER VALOR INPUT Y COMPARAR
-//POSIBLES RESPUESTAS
+function gameHandle(){ 
+    const userNumber = introduceNumber.value;
+    console.log(`La usuaria ha introducido el número ${userNumber}`);
 
+    if (randomNumber === userNumber) {
+        clue.innerHTML = 'Has ganado campeona!!';
+      } else if (
+        userNumber < 1 || userNumber > 100
+      ) {
+        clue.innerHTML = 'Por favor, introduce un número entre 1 y 100.';
+      } else if (randomNumber < userNumber) {
+        clue.innerHTML = 'El número es demasiado alto';
+      } else {
+        clue.innerHTML = 'El número es demasiado bajo';
+      }
+}
+
+
+//ESCRIBIR NUMERO DE INTENTOS
 
 //ESCUCHAR EVENTO
-//tryButtonElement.addEventListener('click',);
+tryButtonElement.addEventListener('click',gameHandle);
