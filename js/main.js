4d928b561e49efb58ae9c57161f7fef205ac5090
Mercedes <mermfb@gmail.com>
2021-01-28 10:06:41 +0100

    Add random number js function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 'use strict';
+//CONSTANTES
+
+const tryButtonElement= document.querySelector('.container__form--button');
+const counterElement= document.querySelector('.container__attempt');
+const randomNumber= getRandomNumber(100);
+
+//ESCRIBIR NUMERO DE INTENTOS
+
+//GENERAR NÚMERO ALEATORIO
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    }
+    console.log(`El número aleatorio es ${randomNumber}`);
+
+//LEER VALOR INPUT Y COMPARAR
+//POSIBLES RESPUESTAS
+
+
+//ESCUCHAR EVENTO
+//tryButtonElement.addEventListener('click',);
