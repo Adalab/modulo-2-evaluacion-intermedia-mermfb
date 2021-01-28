9e6d0cbae246b71d6475d3a42a431a502455ee89
Mercedes <mermfb@gmail.com>
2021-01-28 11:09:43 +0100

    Add preventdefault

--- js/main.js
+++ js/main.js
@@ -5,9 +5,8 @@ const tryButtonElement= document.querySelector('.container__form--button');
 const counterElement= document.querySelector('.container__attempt');
 const randomNumber= getRandomNumber(100);
 const introduceNumber= document.querySelector('.container__form--number');
-const clue= document.querySelector('.container__clue');
-
-
+const clueElement= document.querySelector('.container__clue');
+const  attemptElement= document.querySelector('.container__attempt');
 
 
 //GENERAR NÚMERO ALEATORIO
@@ -18,25 +17,29 @@ function getRandomNumber(max) {
     console.log(`El número aleatorio es ${randomNumber}`);
 
 //LEER VALOR INPUT Y COMPARAR
-function gameHandle(){ 
+function handleGame(){ 
     const userNumber = introduceNumber.value;
     console.log(`La usuaria ha introducido el número ${userNumber}`);
 
     if (randomNumber === userNumber) {
-        clue.innerHTML = 'Has ganado campeona!!';
+        clueElement.innerHTML = 'Has ganado campeona!!';
       } else if (
         userNumber < 1 || userNumber > 100
       ) {
-        clue.innerHTML = 'Por favor, introduce un número entre 1 y 100.';
+        clueElement.innerHTML = 'Por favor, introduce un número entre 1 y 100.';
       } else if (randomNumber < userNumber) {
-        clue.innerHTML = 'El número es demasiado alto';
+        clueElement.innerHTML = 'El número es demasiado alto';
       } else {
-        clue.innerHTML = 'El número es demasiado bajo';
+        clueElement.innerHTML = 'El número es demasiado bajo';
       }
 }
 
 
 //ESCRIBIR NUMERO DE INTENTOS
 
+
+
+
 //ESCUCHAR EVENTO
-tryButtonElement.addEventListener('click',gameHandle);
+tryButtonElement.addEventListener('click',handleGame);
+tryButtonElement.addEventListener('click',handleGame);
